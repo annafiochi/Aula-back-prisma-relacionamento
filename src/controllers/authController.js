f51a0d80efba519a1e6a3125e71c7bbc378032b2
anna <acfiochi@gmail.com>
2025-04-29 11:01:02 -0300

    feat: create login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 import UserModel from '../models/userModel.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 class AuthController {
     // Listar todos os usuários
@@ -48,6 +49,50 @@ async register(req, res) {
         res.status(500).json({ error: "Erro ao criar usuário" });
     }
 }
+    async login(req, res) {
+        try {
+            const { email, password } = req.body;
+
+            if (!email || !password) {
+                return res.status(400).json({ error: 'Os campos email e senha são obrigatórios' });
+            }
+
+            const userExists = await UserModel.findByEmail(email);
+            if (!userExists) {
+                return res.status(400).json({ error: 'Credenciais inválidas' });
+            }
+
+            const isPasswordValid = await bcrypt.compare(password, userExists.password);
+            if (!isPasswordValid) {
+                return res.status(400).json({ error: 'Credenciais inválidas' });
+            }
+
+            const token = jwt.sign(
+                { 
+                    id: userExists.id,
+                    name: userExists.name,
+                    email: userExists.email 
+                },
+                process.env.JWT_SECRET,
+                {
+                    expiresIn: '24h' // O token expira em 24 horas
+                }
+            );
+
+            return res.json({
+                message: 'Login realizado com sucesso',
+                token,
+                user: {
+                    id: userExists.id,
+                    name: userExists.name,
+                    email: userExists.email
+                }
+            });
+        } catch (error) {
+            console.error("Erro ao fazer login:", error);
+            res.status(500).json({ error: "Erro ao fazer login" });
+        }
+    }
 }
 
 export default new AuthController();
